3e90e04eee138747e1d3b030972b662e71b15692
Ed Glazenski <eglazenski@gmail.com>
2020-12-09 18:11:23 -0500

    Add unit tests

---
+++ DataStructures.UnitTests/ArrayManagerTests.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using DataStructures;
+
+namespace DataStructures.UnitTests
+{
+    public class ArrayManagerTests
+    {
+        [Theory]
+        [InlineData(0)]
+        [InlineData(3)]
+        [InlineData(5)]
+        public void Create_ShouldReturnArrayOfArgumentLength_WhenValidLengthIsPassed(int length)
+        {
+            // Arrange
+            var arrayManager = new ArrayManager<int>();
+
+            // Act
+            var result = arrayManager.Create(length);
+
+            // Assert
+            Assert.Equal(length, result.Length);
+        }
+
+        [Theory]
+        [InlineData(-1)]
+        [InlineData(-99)]
+        [InlineData(-5)]
+        public void Create_ShouldThrowArgumentOutOfRangeException_WhenInvalidLengthArgPassed(int length)
+        {
+            // Arrange
+            var arrayManager = new ArrayManager<int>();
+
+            // Assert
+            Assert.Throws<ArgumentOutOfRangeException>(() => arrayManager.Create(length));
+        }
+
+        [Fact]
+        public void Increase_ShouldIncreaseLengthByOne_WhenValidArrayArgPassed()
+        {
+            // Arrange
+            var arrayManager = new ArrayManager<int>();
+            var testArray = new int[] { 1, 2, 3 };
+
+            // Act
+            var result = arrayManager.Increase(testArray);
+
+            // Act
+            Assert.Equal(testArray.Length, result.Length - 1);
+        }
+
+        [Fact]
+        public void Increase_ShouldThrowArgNullException_WhenArgIsNull()
+        {
+            // Arrange
+            var arrayManager = new ArrayManager<int>();
+            var testArray = (int[])null;
+
+            // Assert
+            Assert.Throws<ArgumentNullException>(() => arrayManager.Increase(testArray));
+        }
+    }
+}
---
+++ DataStructures/ArrayManager.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStructures
+{
+    public class ArrayManager<T>
+    {
+        public T[] Create(int length)
+        {
+            if (length < 0)
+                throw new ArgumentOutOfRangeException("Length must be greater than or equal to zero");
+
+            return new T[length];
+        }
+
+        public T[] Increase(T[] array)
+        {
+            if (array == null)
+                throw new ArgumentNullException("Argument cannot be null");
+
+            Array.Resize(ref array, array.Length + 1);
+
+            return array;
+        }
+
+        public T[] Decrease(T[] array)
+        {
+            if (array == null)
+                throw new ArgumentNullException("Argument cannot be null");
+
+            Array.Resize(ref array, array.Length - 1);
+
+            return array;
+        }
+
+        public T[] Append(T[] array, T value)
+        {
+            if (value == null)
+                throw new ArgumentNullException("Argument cannot be null.");
+
+            array = Increase(array);
+            array[array.Length - 1] = value;
+
+            return array;
+        }
+    }
+}
